cc02385d0577e17bd9daf550d09c6906eef05290
Irena Neidlin <XsolarXwhisperX@gmail.com>
2017-12-17 17:09:34 +0200

    added form

--- script.js
+++ script.js
@@ -4,5 +4,22 @@
 var openForm = function(){
     var element = document.getElementById("form");
     element.classList.toggle("opened");
-
 };
+
+document.getElementById('button').addEventListener('click', function(e){
+
+    e.preventDefault();
+
+    var nameinfo = document.getElementById("name");
+    var emailinfo = document.getElementById("email");
+
+    if(!nameinfo.value){
+        nameinfo.style.border = '1px solid red';
+        nameinfo.placeholder = 'add name!';
+    }
+    if(!emailinfo.value){
+        emailinfo.style.border = '1px solid red';
+        emailinfo.placeholder = 'add email!';
+    }
+
+});
